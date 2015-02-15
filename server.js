7ee8e5f8ecea713bdc33ca1fc7de0f607330fb33
Splice09 <jpf0005@gmail.com>
2015-02-14 20:35:07 -0600

    Preparing for server deploy to heroku

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ fileSys = require("fs"),
 app = connect(),
 port = 3000;
 
+app.use('/', function(request, response){
+    var full_path = path.join(process.cwd(), 'index.html');
+    path.exists(full_path,function(exists){
+        if(!exists){
+            response.writeHeader(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+        }
+        else{
+            fileSys.readFile(full_path, "binary", function(err, file){
+                if(err){
+                    response.writeHeader(500, {"Content-Type": "text/plain"});
+                    response.write(err + "\n");
+                    response.end();
+                }
+                else{
+                    response.writeHeader(200);
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
 
 app.use(function(request, response){
     var my_path = url.parse(request.url).pathname;

7dae683397dc2b795516a743a4126ea76d2ec772
Splice09 <jpf0005@gmail.com>
2015-02-14 21:10:10 -0600

    Changed port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ path = require("path"),
 url = require("url"),
 fileSys = require("fs"),
 app = connect(),
-port = 3000;
+port = 8080;
 
 app.use('/', function(request, response){
     var full_path = path.join(process.cwd(), 'index.html');

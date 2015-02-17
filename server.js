974479ae83d63c692367b459760da27d65a3b1d0
Splice09 <jpf0005@gmail.com>
2015-02-16 22:39:15 -0600

    Switching which request function processes '/' requests.

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ app = connect(),
 port = process.env.PORT || 5000,
 htmlPath = "WebFrame.html";
 
-app.use('/', function(request, response){
+app.use('assets/', function(request, response){
     console.log("you are in the first app.use() function");
+    var my_path = url.parse(request.url).pathname;
+    console.log(path.join(url.parse(request.url).pathname, "This is the requested path"));
+    console.log("The full path is ---")
+    var fullPath = path.join(process.cwd(), my_path);
+    console.log(fullPath);
 
-    var fullPath = path.join(process.cwd(), htmlPath);
 
     path.exists(fullPath,function(exists){
         if(!exists){
@@ -35,13 +39,9 @@ app.use('/', function(request, response){
     });
 });
 
-app.use('assets/', function(request, response){
+app.use('/', function(request, response){
     console.log("you are in the second app.use() function");
-    var my_path = url.parse(request.url).pathname;
-    console.log(path.join(url.parse(request.url).pathname, "This is the requested path"));
-    console.log("The full path is ---")
-    var fullPath = path.join(process.cwd(), my_path);
-    console.log(fullPath);
+    var fullPath = path.join(process.cwd(), htmlPath);
     path.exists(fullPath,function(exists){
         if(!exists){
             response.writeHeader(404, {"Content-Type": "text/plain"});

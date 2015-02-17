b98944a9ec95af7ad1376bfa6f2e7360b8cced18
Splice09 <jpf0005@gmail.com>
2015-02-16 21:26:09 -0600

    More debugging statements!

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app = connect(),
 port = process.env.PORT || 5000;
 
 app.use('/', function(request, response){
-    console.log(path.join(process.cwd(), "this is my path"));
+    console.log("you are in the first app.use() function");
+    console.log(path.join(url.parse(request.url).pathname, "This is the requested path"));
+    console.log(path.join(process.cwd(), "this is my path with process.cwd()"));
     var full_path = path.join(process.cwd(), "WebFrame.html");
     path.exists(full_path,function(exists){
         if(!exists){
@@ -33,9 +35,11 @@ app.use('/', function(request, response){
 });
 
 app.use('/assets/', function(request, response){
+    console.log("you are in the second app.use() function");
     var my_path = url.parse(request.url).pathname;
+    console.log(path.join(url.parse(request.url).pathname, "This is the requested path"));
     var full_path = path.join(process.cwd(), my_path);
-    console.log(path.join(full_path, "this is my path."));
+    console.log(path.join(full_path, "this is my full path."));
     path.exists(full_path,function(exists){
         if(!exists){
             response.writeHeader(404, {"Content-Type": "text/plain"});

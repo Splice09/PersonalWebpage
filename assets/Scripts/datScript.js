7ea811f1a8bde34be0b13ded4c8d7dd9840df65d
Splice09 <jpf0005@gmail.com>
2015-10-22 01:48:37 -0500

    adding console logs for debug

--- assets/Scripts/datScript.js
+++ assets/Scripts/datScript.js
@@ -9,3 +9,23 @@ $(document).ready(function()
         }
     );
 });
+
+<script>
+var myTable = "<table><tr><td>Project Name</td>";
+myTable+= "<td>Project Description</td></tr>";
+
+if(projNames.length == 0){
+    console.log("SOMETHING IS UP WITH OUR DATABASE READ IN");
+    }
+else if(projNames.length == projDesc.length){
+    console.log("=========== project names info =======" + projectNames[0]);
+    for(var i = 0; i < projNames.length; i++){
+    myTable+= "<tr><td>" + projectNames[i] + "</td><td>" + projectDesc[i] + "</td></tr>";
+    }
+}
+else{
+    console.log("table arrays aren't the same length.")
+    }
+myTabe+= "</table>";
+document.getElementsByClassName(ppContent).innerHTML(myTable);
+</script>

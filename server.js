249b1bef227d0fd050829f1e214527fa8e3aad22
Splice09 <jpf0005@gmail.com>
2015-10-22 01:07:43 -0500

    attempting to draw table dynamically from database

--- server.js
+++ server.js
@@ -99,14 +99,19 @@ console.log('Connected via port ' + port);
 
 //Connect to DATABASE_URL (heroku postgreSQL database)
 var connectionString = "postgres://xppbneritkkeqc:ORqdupmaW39VMbGad0hzgZVC-i@ec2-54-225-201-25.compute-1.amazonaws.com:5432/d34n1n2r66gvkb";
+var projNames = [];
+var projDesc = [];
 pg.connect(connectionString, function(err, client) {
     if (err) throw err;
     console.log('Connected to postgres! Getting schemas...');
 
-    client
-        //change code to match the schema and table name in my database
-        .query('SELECT pastProjects FROM projects.pastProjects;')
+    var nameQuery = client.query('SELECT projname FROM projects.pastProjects;');
+    var descQuery = client.query('SELECT projdesc FROM projects.pastProjects;')
+        nameQuery.on('row', function(row) {
+            projNames.push(row);
+        });
+        descQuery.query('SELECT projdesc FROM projects.pastProjects;')
         .on('row', function(row) {
-            console.log(JSON.stringify(row));
+            projDesc.push(row);
         });
 });

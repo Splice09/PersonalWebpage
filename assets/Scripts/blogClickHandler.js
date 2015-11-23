ae344990d1979e44ad290ccbdb97cc0dfb126f0a
Splice09 <jpf0005@gmail.com>
2015-11-23 01:27:42 -0600

    Testing if I can make blogNumbers clickable

--- assets/Scripts/blogClickHandler.js
+++ assets/Scripts/blogClickHandler.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
      Listens for a blog page number to be clicked and then changes the content of current work to
      match the number clicked
      */
-    $(".blogNumber").click(function(event){
-        var numberID = event.target.id;
+    $('.blogNumber').click(function(){
+        //var numberID = event.target.id;
         //$('.cwContent').empty();
         alert("click is working");
         //$('.cwContent').html(blogArray[numberID]);

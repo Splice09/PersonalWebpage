39f2b6b538e9d676391c3a140b847f6bcbb57e87
Splice09 <jpf0005@gmail.com>
2015-11-23 00:30:43 -0600

    changed code to display blog post using javascript and postgres query instead of hard HTML code

--- assets/Scripts/currentWork.js
+++ assets/Scripts/currentWork.js
@@ -1,9 +1,13 @@
+var blogArray;
+
 function getWorkData(pageName){
     var posting = $.post("public/node_modules/server.js", {
         page_name: pageName
     },function(data){},"json");
 
     posting.done(function(cdata){
+        blogArray = cdata;
+        $('.cwContent').html(cdata[0]);
         for(var i = 0; i < cdata.length; i++){
             var tempNum = i + 1;
             var tempString = "<p class=\"blogNumber\" id=\"" + i + "\">" + tempNum + "</p>";

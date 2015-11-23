240dcdeda37beb77b6a8e9b93f51968f7c9195b6
Splice09 <jpf0005@gmail.com>
2015-11-23 01:33:30 -0600

    Changing jquery from .click() to .on() because class is dynamically generated

--- assets/Scripts/blogClickHandler.js
+++ assets/Scripts/blogClickHandler.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
      Listens for a blog page number to be clicked and then changes the content of current work to
      match the number clicked
      */
-    $('.blogNumber').click(function(){
-        //var numberID = event.target.id;
+    $('.blogNumber').on("click", function(event){
+        var numberID = event.target.id;
         //$('.cwContent').empty();
-        alert("click is working");
+        alert(numberID);
         //$('.cwContent').html(blogArray[numberID]);
     });
 });
